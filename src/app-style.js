286f8d8903cabf59c83ea0914b1ef35aa8d67650
JoaoJorge <joaojorgedosanjos@gmail.com>
2023-01-08 10:20:02 -0300

    criação global style

---
+++ src/app-style.js
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from 'styled-components';
+import  Bc  from "./components/img/bc.png"
+
+export const GlobalStyle = createGlobalStyle`
+*{
+    margin:0;
+    padding:0;
+    box-sizing: border-box;
+  }
+
+
+  body{
+      background-image:url(${Bc});
+      background-size: cover;
+      background-position: center;
+      background-repeat: no-repeat;
+      height: 100vh;
+      width: 100%;
+      font-family: 'Roboto', sans-serif;
+      
+
+  }
+  a{
+    text-decoration: none;
+  }
+  li{
+    list-style:none;
+  }
+  `
+
