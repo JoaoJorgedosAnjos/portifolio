c7ff11bb1bbbe8bfc332334183f0f2055fca6bdf
JoaoJorge <joaojorgedosanjos@gmail.com>
2023-01-07 22:58:09 -0300

    correção bc

--- src/components/main/style.js
+++ src/components/main/style.js
@@ -2,7 +2,7 @@ import styled from "styled-components"
 
 export const Container = styled.main`
 width:100;
-height:79.4vh;
+height:80vh;
 display:flex;
 justify-content:center;
 align-items: center;
