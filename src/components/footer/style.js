27da6c3148c5dd66673d7fded5b6866a2b321e30
JoaoJorge <joaojorgedosanjos@gmail.com>
2023-01-07 22:54:40 -0300

    correção bc

--- src/components/footer/style.js
+++ src/components/footer/style.js
@@ -2,8 +2,7 @@ import styled from "styled-components"
 
 export const Container = styled.footer`
 width:100%;
-height:118px;
-//height:100%;
+height:100%;
 color:white;
 display:flex;
 justify-content:center;
