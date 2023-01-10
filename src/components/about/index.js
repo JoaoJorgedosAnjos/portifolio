2bd4cf785c19b896ca2b594fcfe9b3da04263255
JoaoJorge <joaojorgedosanjos@gmail.com>
2023-01-10 18:46:14 -0300

    att conteúdo aba sobre

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,14 +1,19 @@
 import * as S from "./style"
+import Pc from "../img/pc.gif"
 
 export const Main = () => {
     return (
-            <S.Container>
-                <div>
-                    <h1>Eu sou <span>João Jorge</span></h1>
-                    <h2>Desenvolvedor <span>Front-end</span></h2>
-                </div>
-                <div>
-                </div>
-            </S.Container>       
+        <S.Container>
+            <div>
+                <img src={Pc} />
+            </div>
+            <section>
+                <h1>Olá! <span>Seja bem-vindo ao meu portifólio.</span> Me chamo João Jorge, sou desenvolvedor Front-end em formação.</h1>
+                <p>Sempre fui muito interessado e curioso com a área de tecnologia. Gosto muito de pesquisar e entender sobre como elas funcionam. Tenho certeza de que posso contribuir de várias formas com qualquer time por ser bastante focado, esforçado e também porque sou muito responsável com meu trabalho, ainda mais quando estou trabalhando em equipe. </p>
+                <p>Estou sempre estudando e colocando novos projetos em prática, como também revisando conteúdos que vi há mais tempo. Busco me atualizar, tanto na parte dos estudos e prática, quanto na participação de comunidades de tecnologia. Ouvindo podcast, participando de comunidades em diversas redes sociais. Tento contribuir com o que sei e ficar antenado nas novidades.</p>
+                <p>Além de ser desenvolvedor também sou músico, guitarrista e violonista e professor. Também sou apaixonado por jogos de videogames, animes e mangas, e tecnologias em geral.</p>
+                <a href="https://drive.google.com/file/d/1FOegq2ITDOwRd4aNfxpYJEml4K3HrTGJ/view?usp=sharing" target="_blank">Veja meu CV</a>
+            </section>
+        </S.Container>
     )
 }
--- src/components/about/style.js
+++ src/components/about/style.js
@@ -3,6 +3,74 @@ import styled from "styled-components"
 export const Container = styled.main`
 height:82vh;
 width:100%;
+display:flex;
+justify-content:center;
+align-items:center;
+color:#F5F5F5;
+gap:50px;
+@media (max-width: 975px){
+    height:100%;
+    flex-direction: column-reverse;
+}
 
-color:white;
+div{
+    img{
+        width:450px;
+        border-radius:15px;
+        @media (max-width: 975px){
+            width:350px;
+        }    
+    }
+}
+section{
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    flex-direction:column;
+    gap:15px;
+    width:450px;
+    height:630px;
+    padding:10px;
+    background-color: rgba(25, 25, 25, 0.7);
+    border-radius:20px;
+      
+    @media (max-width: 975px){
+        width:350px;
+        height:820px;   
+    }
+
+    span{
+        color:#8A2BE2;
+    }
+
+    h1{
+        font-size:20px;
+    }
+
+    p{
+        font-size:18px;
+        font-weight:600;
+    }
+
+    a{
+        display:flex;
+        align-items:center;
+        justify-content:center;
+        width:135px;
+        height: 40px;
+        border-radius:20px;
+        background-color:#F5F5F5;
+        color:#8A2BE2;
+        font-size:20px;
+        font-weight:700;
+        &:hover{
+            background-color:#FFBF00;
+            background-color:#8A2BE2;
+            transition:0.5s ease-in-out;
+            color:black;
+            color:#FFBF00;
+            color:white;   
+        }
+    }
+}
 `
