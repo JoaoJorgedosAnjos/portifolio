cce914a6344193f038897e80acf94dfbb2adf962
JoaoJorge <joaojorgedosanjos@gmail.com>
2023-02-07 17:32:42 -0300

    Att link currículo

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -12,7 +12,7 @@ export const Main = () => {
                 <p>Sempre fui muito interessado e curioso com a área de tecnologia. Gosto muito de pesquisar e entender sobre como elas funcionam. Tenho certeza de que posso contribuir de várias formas com qualquer time por ser bastante focado, esforçado e também porque sou muito responsável com meu trabalho, ainda mais quando estou trabalhando em equipe. </p>
                 <p>Estou sempre estudando e colocando novos projetos em prática, como também revisando conteúdos que vi há mais tempo. Busco me atualizar, tanto na parte dos estudos e prática, quanto na participação de comunidades de tecnologia. Ouvindo podcast, participando de comunidades em diversas redes sociais. Tento contribuir com o que sei e ficar antenado nas novidades.</p>
                 <p>Além de ser desenvolvedor também sou músico, guitarrista, violonista e professor. Também sou apaixonado por jogos de videogames, animes e mangas, e tecnologias em geral.</p>
-                <a href="https://drive.google.com/file/d/19nmV3Y9vxQQmi0Oi1nff2FYsOi2xFYk4/view?usp=share_link" target="_blank" rel="noreferrer">Veja meu CV</a>
+                <a href="https://drive.google.com/file/d/1mHc9esjxs6VvrCmZKvUA7Xsu3SjZa4ir/view?usp=sharing">Veja meu CV</a>
             </section>
         </S.Container>
     )
